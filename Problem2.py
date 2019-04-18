98fc46e6f7d2fc46fbc7340b60244c69cabf9cd3
Sarah Shiraz <48074714+sarahshiraz@users.noreply.github.com>
2019-04-17 20:56:37 -0400

    Problem2 of assignment3

---
+++ Problem2.py
@@ -0,0 +1,181 @@
+import os
+from keras.applications.vgg19 import VGG19
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+
+base_dir = '/home/shiraz/Desktop/ML/Assignment3/cats_and_dogs_filtered'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+from keras.applications import VGG19
+
+conv_base = VGG19(
+    weights='imagenet',
+    include_top=False,
+    input_shape=(150, 150, 3))
+
+conv_base.trainable = False
+
+#Concatenate the convolutional base and densely connected layers
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+#model.add(layers.Dropout(0.1))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+
+#Train the model end to end with frozen convolutional base
+# data augmentation
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+# compile model
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=optimizers.RMSprop(lr=2e-5),
+    metrics=['acc'])
+
+# train
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=10,
+    validation_data=validation_generator,
+    validation_steps=50
+)
+
+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# training and validation accuracy
+
+plt.plot(epochs, acc, 'bo', label='training acc')
+plt.plot(epochs, val_acc, 'b', label='validation acc')
+plt.title('training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+# training and validation loss
+
+plt.plot(epochs, loss, 'bo', label='training loss')
+plt.plot(epochs, val_loss, 'b', label='validation loss')
+plt.title('training and validation loss')
+plt.legend()
+
+plt.show()
+
+# Fine tuning
+conv_base.trainable = True
+
+set_trainable = False
+for layer in conv_base.layers:
+  if layer.name == 'block5_conv4':
+    set_trainable = True
+  if set_trainable:
+    layer.trainable = True
+  else:
+    layer.trainable = False
+
+
+# compile model
+
+model.compile(
+    loss='binary_crossentropy',
+    #
+    # choose a smaller learning rate
+    #
+    optimizer=optimizers.RMSprop(lr=1e-5),
+    metrics=['acc'])
+
+# train
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=10,
+    validation_data=validation_generator,
+    validation_steps=50)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# training and validation accuracy
+
+plt.plot(epochs, acc, 'bo', label='training acc')
+plt.plot(epochs, val_acc, 'b', label='validation acc')
+plt.title('training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+# training and validation loss
+
+plt.plot(epochs, loss, 'bo', label='training loss')
+plt.plot(epochs, val_loss, 'b', label='validation loss')
+plt.title('training and validation loss')
+plt.legend()
+
+plt.show()
+
+# Print out validation loss and accuracy
+
+val_loss, val_acc = model.evaluate_generator(validation_generator, steps=50)
+print("Validation loss:", val_loss)
+print("Validation accuracy:", val_acc)
+
+model_fname = 'cats_and_dogs_small_4_problem2_vgg19.h5'
+model.save(model_fname)
